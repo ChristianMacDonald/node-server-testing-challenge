71cbc12e15b5204c7b0907469b119370f8247f00
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-29 22:17:34 -0400

    Created fruitModel.js

---
+++ fruits/fruitModel.js
@@ -0,0 +1,32 @@
+const db = require('../data/dbConfig');
+
+function get(id = 0) {
+    if (id) {
+        return db('fruits').where({ id }).first();
+    } else {
+        return db('fruits');
+    }
+}
+
+async function insert(fruit) {
+    let [id] = await db('fruits').insert(fruit);
+    return get(id);
+}
+
+async function update(id, changes) {
+    await db('fruits').where({ id }).update(changes);
+    return get(id);
+}
+
+async function remove(id) {
+    let fruit = get(id);
+    await db('fruits').where({ id }).del();
+    return fruit;
+}
+
+module.exports = {
+    get,
+    insert,
+    update,
+    remove
+};
