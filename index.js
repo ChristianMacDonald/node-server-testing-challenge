523e18887de220faef77ba8bebc536102738314e
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-29 22:35:17 -0400

    Created fruitRouter.js and added all necessary endpoints to server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const fruitRouter = require('./fruits/fruitRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use('/api/fruits', fruitRouter);
 
 server.listen(8000, () => {
     console.log('Server listening on port 8000.');
