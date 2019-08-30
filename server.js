93876a58df66e2e1318e48fbd9ceccf810e559aa
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-30 00:57:26 -0400

    Added more tests and deleted extraneous index route test

--- server.js
+++ server.js
@@ -6,8 +6,4 @@ const server = express();
 server.use(express.json());
 server.use('/api/fruits', fruitRouter);
 
-server.get('/', (req, res) => {
-    res.status(200).json({ api: 'running' });
-});
-
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -8,32 +8,55 @@ beforeEach(async () => {
 });
 
 describe('server.js', () => {
-    describe('index route', () => {
-        it('has process.env.DB_ENV as testing', () => {
-            expect(process.env.DB_ENV).toBe('testing');
-        });
-        it('should return an OK status code from the index route', async () => {
-            let expectedStatusCode = 200;
-            let response = await request(server).get('/');
-            expect(response.status).toEqual(expectedStatusCode);
-        });
-        it('should return a JSON obect from the index route', async () => {
-            let expectedBody = { api: 'running' };
-            let response = await request(server).get('/');
-            expect(response.body).toEqual(expectedBody);
-        });
-        it('should return a JSON object from the index route', async () => {
-            let response = await request(server).get('/');
-            expect(response.type).toEqual('application/json');
-        });
-    });
     describe('fruit model', () => {
         describe('insert()', () => {
             it('should insert the provided fruits into the database', async () => {
-                await fruitModel.insert({ name: 'Cranberry' });
-                await fruitModel.insert({ name: 'Grape' });
+                await fruitModel.insert({ name: 'cranberry' });
+                await fruitModel.insert({ name: 'grape' });
                 let fruits = await db('fruits');
                 expect(fruits).toHaveLength(2);
+            });
+        });
+        describe('remove()', () => {
+            it('should remove the inserted fruit from the database', async () => {
+                let [id] = await db('fruits').insert({ name: 'Grape' });
+                await fruitModel.remove(id);
+                let fruits = await db('fruits');
+                expect(fruits).toHaveLength(0);
+            });
+        });
+    });
+    describe('fruits route', () => {
+        describe('POST', () => {
+            it('should return a Created status code', async () => {
+                let response = await request(server).post('/api/fruits').send({ 'name': 'grape' });
+                expect(response.status).toEqual(201);
+            });
+            it('should return a JSON object', async () => {
+                let response = await request(server).post('/api/fruits').send({ 'name': 'grape' });
+                expect(response.type).toEqual('application/json');
+            });
+        });
+        describe('GET', () => {
+            it('should return an OK status code', async () => {
+                let response = await request(server).get('/api/fruits');
+                expect(response.status).toEqual(200);
+            });
+            it('should return a JSON object', async () => {
+                let response = await request(server).get('/api/fruits');
+                expect(response.type).toEqual('application/json');
+            });
+        });
+        describe('DELETE', () => {
+            it('should return an OK status code', async () => {
+                let [id] = await db('fruits').insert({ name: 'grape' });
+                let response = await request(server).delete(`/api/fruits/${id}`);
+                expect(response.status).toEqual(200);
+            });
+            it('should return a JSON object', async () => {
+                let [id] = await db('fruits').insert({ name: 'grape' });
+                let response = await request(server).delete(`/api/fruits/${id}`);
+                expect(response.type).toEqual('application/json');
             });
         });
     });
